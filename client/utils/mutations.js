6e85ab8f5ce792c4cfe2229721fae28c43076e35
Arman <armanbarseghyan83@gmail.com>
2024-01-20 13:09:10 -0800

    mutations.js and queries.js created

---
+++ client/utils/mutations.js
@@ -0,0 +1,58 @@
+import { gql } from '@apollo/client';
+
+export const LOGIN_USER = gql`
+  mutation loginUser($email: String!, $password: String!) {
+    login(email: $email, password: $password) {
+      token
+      user {
+        _id
+        username
+      }
+    }
+  }
+`;
+
+export const ADD_USER = gql`
+  mutation addUser($username: String!, $email: String!, $password: String!) {
+    addUser(username: $username, email: $email, password: $password) {
+      token
+      user {
+        _id
+        username
+      }
+    }
+  }
+`;
+
+export const SAVE_BOOK = gql`
+  mutation saveBook($book: BookInput!) {
+    saveBook(book: $book) {
+      username
+      email
+      savedBooks {
+        bookId
+        image
+        title
+        authors
+        description
+      }
+    }
+  }
+`;
+
+export const REMOVE_BOOK = gql`
+utation removeBook($bookId: String!) {
+  removeBook(bookId: $bookId) {
+    username
+    email
+    savedBooks {
+      bookId
+      image
+      title
+      authors
+      description
+    }
+  }
+  }
+}
+`;
