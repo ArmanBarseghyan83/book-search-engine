b6e09561e5e25c72b9b509e52018238488683f0a
Arman <armanbarseghyan83@gmail.com>
2024-01-21 14:49:28 -0800

    cors installed, server.js updated to use cors

--- server/server.js
+++ server/server.js
@@ -1,6 +1,8 @@
 const express = require('express');
 const { ApolloServer } = require('@apollo/server');
 const { expressMiddleware } = require('@apollo/server/express4');
+const cors = require('cors');
+
 const path = require('path');
 const { authMiddleware } = require('./utils/auth');
 
@@ -9,6 +11,7 @@ const db = require('./config/connection');
 
 const PORT = process.env.PORT || 3001;
 const app = express();
+app.use(cors());
 const server = new ApolloServer({
   typeDefs,
   resolvers,
