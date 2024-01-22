d40bf54b214e92e69bcb4daf0970058dcaa3c55a
Arman <armanbarseghyan83@gmail.com>
2024-01-21 17:08:51 -0800

    dotenv installed, connected to mongodb atlas

--- server/config/connection.js
+++ server/config/connection.js
@@ -1,5 +1,8 @@
+const dotenv = require('dotenv');
+dotenv.config();
+
 const mongoose = require('mongoose');
 
-mongoose.connect(process.env.MONGODB_URI || 'mongodb://127.0.0.1:27017/googlebooks');
+mongoose.connect(process.env.MONGODB_URI);
 
 module.exports = mongoose.connection;
--- server/server.js
+++ server/server.js
@@ -8,7 +8,6 @@ const { authMiddleware } = require('./utils/auth');
 
 const { typeDefs, resolvers } = require('./schemas');
 const db = require('./config/connection');
-
 const PORT = process.env.PORT || 3001;
 const app = express();
 app.use(cors());
@@ -24,9 +23,12 @@ const startApolloServer = async () => {
   app.use(express.urlencoded({ extended: false }));
   app.use(express.json());
 
-  app.use('/graphql', expressMiddleware(server, {
-    context: authMiddleware
-  }));
+  app.use(
+    '/graphql',
+    expressMiddleware(server, {
+      context: authMiddleware,
+    })
+  );
 
   if (process.env.NODE_ENV === 'production') {
     app.use(express.static(path.join(__dirname, '../client/dist')));
@@ -45,4 +47,4 @@ const startApolloServer = async () => {
 };
 
 // Call the async function to start the server
-  startApolloServer();
+startApolloServer();
