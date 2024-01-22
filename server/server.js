ebc8a02d7624d761f4915a2289ced8a1ef32dc32
Arman <armanbarseghyan83@gmail.com>
2024-01-21 17:16:38 -0800

    server.js updated

--- server/server.js
+++ server/server.js
@@ -2,6 +2,8 @@ const express = require('express');
 const { ApolloServer } = require('@apollo/server');
 const { expressMiddleware } = require('@apollo/server/express4');
 const cors = require('cors');
+const dotenv = require('dotenv');
+dotenv.config();
 
 const path = require('path');
 const { authMiddleware } = require('./utils/auth');
